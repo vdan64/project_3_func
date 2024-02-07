68bd20f5f76b5490bc058a930d50140545d1c562
victors <victordaniel64x@gmail.com>
2024-02-07 11:35:11 -0400

    home page added

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import Posts
 # Create your views here.
+
+class HomePageView(ListView):
+    model = Posts
+    template_name = "home.html"
